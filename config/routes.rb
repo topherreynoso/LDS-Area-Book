2640a68bf612bd7998d63e4cc4d87f7f0f658254
Christopher Reynoso <topherreynoso@gmail.com>
2013-05-20 15:50:19 -0700

    added layout with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AreaBook::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

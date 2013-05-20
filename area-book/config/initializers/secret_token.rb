18f1ff4380db3d922ec43e5bb2860463d3c9d163
Christopher Reynoso <topherreynoso@gmail.com>
2013-05-20 12:28:48 -0700

    added rails project

---
+++ area-book/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AreaBook::Application.config.secret_token = 'dac9cdf13193c17ba7cdc1871013944e0ca904a282409fefdb09c008df5f6a1d6ec5dee6632e00d6eae08a4a76cef4f685fb11ae79850fd9889ad05de1237ff1'

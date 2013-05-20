7d2bd5d68208eba2ee8279710e35a31ca6564c45
Christopher Reynoso <topherreynoso@gmail.com>
2013-05-20 15:26:50 -0700

    added static pages

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+
+  describe "GET '...'" do
+    it "returns http success" do
+      get '...'
+      response.should be_success
+    end
+  end
+
+end

9e595156af20d3caadd54c82bd641c91d87d451b
Christopher Reynoso <topherreynoso@gmail.com>
2013-05-20 21:53:02 -0700

    sign in and sign out working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name,  presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +9,10 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 AreaBook::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   root to: 'static_pages#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/signin',  to: 'users#new',            via: 'get'
+
+  match '/help',    to: 'static_pages#help', via: 'get'
+  match '/newuser', to: 'users#new', 		 via: 'get'
+  match '/signin',  to: 'sessions#new', 	 via: 'get'
+  match '/signout', to: 'sessions#destroy',	 via: 'delete'
 end

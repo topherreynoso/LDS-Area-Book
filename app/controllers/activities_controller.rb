f3a6214bcf265a5cdbcf14ad7ed2f8f6be645aec
Christopher Reynoso <topherreynoso@gmail.com>
2013-05-22 04:46:30 -0700

    beefed up security

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,6 @@
 class ActivitiesController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :new, :update, :create, :destroy, :reports, :archive]
+
   def new
   	@activity = Activity.new
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:index, :edit, :new, :update, :create, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy, :index]
+  before_action :admin_user,     only: [:new, :create, :destroy, :index]
 
   def index
     @users = User.paginate(page: params[:page])

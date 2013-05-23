1dea777d05aee527d22e183d6978d4e8144c49b3
Christopher Reynoso <topherreynoso@gmail.com>
2013-05-23 09:05:39 -0700

    still trying to get images in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ AreaBook::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
